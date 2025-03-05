4b33bac8b79a2c9eaadfa8797ece0f20a9ce2874
Shivam Sharma <shivam691999@gmail.com>
2025-03-05 14:53:35 +0530

    support : devnet-4, update elderjs

--- constants.js
+++ constants.js
@@ -7,11 +7,11 @@
 // };
 
 export const ELDER_CHAIN_CONFIG = {
-    chainName: "devnet-2",
+    chainName: "devnet-4",
     rpc: "http://localhost:26657",
     rest: "http://localhost:1317",
-    rollID: 2,
-    rollChainID: 1512,
+    rollID: 3,
+    rollChainID: 42069,
 
     eth_rpc : "http://localhost:8545",
 };
